e76d4f12968f2292e79669920c3d5cb4ffa31553
IMU <IMU@DESKTOP-FSRQE0S>
2023-04-25 15:50:49 +0630

    First Time

---
+++ IMUStudentManagementSystem/src/studentinfo/pac1/Student.java
@@ -0,0 +1,10 @@
+package studentinfo.pac1;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome from IMU Student Management System");
+	}
+
+}
